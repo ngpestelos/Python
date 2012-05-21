13f4ecb4f5331b2f88aa940daaf4f100ea750cc8
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2012-05-21 14:24:32 +0800

    crawl pages after some depth

---
+++ cs101/hw3/maxdepth.py
@@ -0,0 +1,118 @@
+# The web crawler we built at the end of Unit 2 has some serious
+# flaws if we were going to use it in a real crawler. One
+# problem is if we start with a good seed page, it might
+# run for an extremely long time (even forever, since the
+# number of URLS on the web is not actually finite). The final two
+# questions of the homework ask you to explore two different ways
+# to limit the pages that it can crawl.
+
+def get_page(url):
+    try:
+        if url == "http://www.udacity.com/cs101x/index.html":
+            return ('<html> <body> This is a test page for learning to crawl! '
+            '<p> It is a good idea to '
+            '<a href="http://www.udacity.com/cs101x/crawling.html">learn to '
+            'crawl</a> before you try to  '
+            '<a href="http://www.udacity.com/cs101x/walking.html">walk</a> '
+            'or  <a href="http://www.udacity.com/cs101x/flying.html">fly</a>. '
+            '</p> </body> </html> ')
+        elif url == "http://www.udacity.com/cs101x/crawling.html":
+            return ('<html> <body> I have not learned to crawl yet, but I '
+            'am quite good at '
+            '<a href="http://www.udacity.com/cs101x/kicking.html">kicking</a>.'
+            '</body> </html>')
+        elif url == "http://www.udacity.com/cs101x/walking.html":
+            return ('<html> <body> I cant get enough '
+            '<a href="http://www.udacity.com/cs101x/index.html">crawling</a>! '
+            '</body> </html>')
+        elif url == "http://www.udacity.com/cs101x/flying.html":
+            return ('<html> <body> The magic words are Squeamish Ossifrage! '
+            '</body> </html>')
+        elif url == "http://top.contributors/velak.html":
+            return ('<a href="http://top.contributors/jesyspa.html">'
+        '<a href="http://top.contributors/forbiddenvoid.html">')
+        elif url == "http://top.contributors/jesyspa.html":
+            return  ('<a href="http://top.contributors/elssar.html">'
+        '<a href="http://top.contributors/kilaws.html">')
+        elif url == "http://top.contributors/forbiddenvoid.html":
+            return ('<a href="http://top.contributors/charlzz.html">'
+        '<a href="http://top.contributors/johang.html">'
+        '<a href="http://top.contributors/graemeblake.html">')
+        elif url == "http://top.contributors/kilaws.html":
+            return ('<a href="http://top.contributors/tomvandenbosch.html">'
+        '<a href="http://top.contributors/mathprof.html">')
+        elif url == "http://top.contributors/graemeblake.html":
+            return ('<a href="http://top.contributors/dreyescat.html">'
+        '<a href="http://top.contributors/angel.html">')
+        elif url == "A1":
+            return  '<a href="B1"> <a href="C1">  '
+        elif url == "B1":
+            return  '<a href="E1">'
+        elif url == "C1":
+            return '<a href="D1">'
+        elif url == "D1":
+            return '<a href="E1"> '
+        elif url == "E1":
+            return '<a href="F1"> '
+    except:
+        return ""
+    return ""
+
+def get_next_target(page):
+    start_link = page.find('<a href=')
+    if start_link == -1:
+        return None, 0
+    start_quote = page.find('"', start_link)
+    end_quote = page.find('"', start_quote + 1)
+    url = page[start_quote + 1:end_quote]
+    return url, end_quote
+
+def union(p,q):
+    for e in q:
+        if e not in p:
+            p.append(e)
+
+def get_all_links(page):
+    links = []
+    while True:
+        url,endpos = get_next_target(page)
+        if url:
+            links.append(url)
+            page = page[endpos:]
+        else:
+            break
+    return links
+
+def crawl_web(seed, max_depth):
+	tocrawl = [seed]
+	crawled = []
+	next_depth = [] # links at next depth
+	depth = 0
+	while tocrawl and depth <= max_depth:
+		page = tocrawl.pop()
+		if page not in crawled:
+			union(next_depth, get_all_links(get_page(page)))
+			crawled.append(page)
+		if not tocrawl:
+			tocrawl, next_depth = next_depth, []
+			depth += 1
+	return crawled
+
+#def crawl_web(seed, max_depth):
+#	tocrawl = [[seed]]
+#	crawled = []
+#	depth = 0
+#	while tocrawl:
+#		if depth > max_depth:
+#			break
+#		pages = tocrawl.pop()
+#		links_at_depth = []
+#		for page in pages:
+#			links = get_all_links(get_page(page))
+#			union(links_at_depth, links)
+#			crawled.append(page)
+#		tocrawl.append(links_at_depth)
+#		depth += 1
+#	return crawled
+
+print crawl_web("http://www.udacity.com/cs101x/index.html", 100)
