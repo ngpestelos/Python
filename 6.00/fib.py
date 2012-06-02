5f6945fa4193a6c8fcfd65f523874e8127abaa9e
Nestor G Pestelos Jr <ngpestelos@gmail.com>
2012-06-02 12:45:31 +0800

    added samples from MIT 6.00

---
+++ 6.00/fib.py
@@ -0,0 +1,15 @@
+def fib(n):
+    if n <= 1:
+        return 1
+    else:
+        return fib(n-1) + fib(n-2)
+
+print fib(0)
+print fib(1)
+print fib(2)
+print fib(3)
+print fib(4)
+print fib(5)
+print fib(6)
+print fib(7)
+print fib(8)
